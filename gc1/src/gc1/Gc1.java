49ff94e9fec0247152683b20c6c38289738564ee
mwmuller <mwmuller@oakland.edu>
2017-10-03 20:56:28 -0400

    New clients

---
+++ gc1/src/gc1/Gc1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gc1;
+
+import java.net.*;
+
+class GameClient {
+
+    private static Socket socket;
+    private static ServerThread serv_thread;
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+            socket = new Socket("localhost", 1234);
+            serv_thread = new ServerThread(900, 500, "Game and Chat Hub");
+            serv_thread = new ServerThread(socket, "");
+        } catch (Exception e) {
+            System.out.println("There was an issue using the current port.");
+            System.exit(-1);
+        }
+    }
+
+}
